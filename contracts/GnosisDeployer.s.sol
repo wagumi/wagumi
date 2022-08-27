aff0f4009338391ffb202f563ba8079dc10424e5
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-26 20:50:31 -0700

    chore: update

--- contracts/GnosisDeployer.s.sol
+++ contracts/GnosisDeployer.s.sol
@@ -17,13 +17,17 @@ contract GnosisScript is Script {
   address GNOSIS_COMPATIBILITY_FALLBACK_HANDLER =
     address(0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4);
 
-  address[] internal owners = new address[](1);
+  address[] internal owners = new address[](5);
   GnosisSafeProxy proxy;
   GnosisSafe safe;
   GnosisSafeL2 public safeL2;
 
   function run() external {
-    owners[0] = address(0xA5A7468f177d94212cd0FDC0886EE732155c47e9);
+    owners[0] = address(0x4fd9D0eE6D6564E80A9Ee00c0163fC952d0A45Ed);
+    owners[1] = address(0xE3EF6f44C0aEcB8CF518a85D2739021aa3Cd02d9);
+    owners[2] = address(0xa4dDE78906b73133296D43f23Ca4b41009059123);
+    owners[3] = address(0xa3C2e2fBF83aB8e690c4E7e1C30Fa3778B7E4E1e);
+    owners[4] = address(0x19a351b9F2991a3dd71f4ca38f98ad6F418Ed9ea);
 
     GnosisSafeProxyFactory factory = GnosisSafeProxyFactory(
       GNOSIS_PROXY_FACTORY_ADDRESS_1_3_0
@@ -36,7 +40,7 @@ contract GnosisScript is Script {
         abi.encodeWithSignature(
           "setup(address[],uint256,address,bytes,address,address,uint256,address)",
           owners,
-          1,
+          2,
           address(0),
           "",
           GNOSIS_COMPATIBILITY_FALLBACK_HANDLER,
@@ -53,7 +57,7 @@ contract GnosisScript is Script {
         abi.encodeWithSignature(
           "setup(address[],uint256,address,bytes,address,address,uint256,address)",
           owners,
-          1,
+          2,
           address(0),
           "",
           GNOSIS_COMPATIBILITY_FALLBACK_HANDLER,
