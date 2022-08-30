cb751ef6054c6cd58323325736373590a01643ab
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-29 20:32:30 -0700

    chore: update

--- contracts/WagumiMigrationProposal.sol
+++ contracts/WagumiMigrationProposal.sol
@@ -6,12 +6,32 @@ pragma solidity ^0.8.13;
 
 contract WagumiMigrationProposal {
   address public OLD_MULTI_SIG =
-    address(0xdce4694e268bd83ea41b335320ed11a684a1d7db);
+    address(0xDCE4694e268bD83EA41B335320Ed11A684a1d7dB);
+  address public NEW_MULTI_SIG =
+    address(0xEA7e6C3D85804E220239D395C6B7aD1aEff8c216);
+  address public WAGUMI_CATS_CONTRACT =
+    address(0x6144D927EE371de7e7f8221b596F3432E7A8e6D9);
+
+  address public ENS_CONTRACT =
+    address(0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85);
+  uint256 public WAGUMI_ETH_TOKEN_ID =
+    18019805752382518475808758214599099996488809401149089142030556561887882232265;
 
   function run() external {
     for (uint256 i = 0; i < 11; i++) {
-      _safeMint(minter, (_currentId + i));
-      supplyCounter.increment();
+      IERC721(WAGUMI_CATS_CONTRACT).transferFrom(
+        address(this),
+        NEW_MULTI_SIG,
+        i
+      );
     }
+
+    IERC721(ENS_CONTRACT).transferFrom(
+      address(this),
+      NEW_MULTI_SIG,
+      WAGUMI_ETH_TOKEN_ID
+    );
+
+    NEW_MULTI_SIG.call{ value: address(this).balance }("");
   }
 }
